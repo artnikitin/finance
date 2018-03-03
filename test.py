0296b0519347d40550a954e18d471a2cd34d3ae9
Artem Nikitin <artnikitin@gmail.com>
2018-03-03 21:38:19 +0000

    new commit

--- test.py
+++ test.py
@@ -1,20 +1,9 @@
 from cs50 import SQL
-from helpers import *
-# configure CS50 Library to use SQLite database
+
 db = SQL("sqlite:///finance.db")
 
-update_cash = lookup("GOOG")
+username = "tim"
+hash = "$6$rounds=656000$X45uEcVzvzqZZF7M$JHvoMRPfDSyW2pv1e5g39FO6ckIM2JiYsd9IyI0H9qmrHgu7.OwLJHio1iTSpQsFLZhai/nStfJ63ypJgRFem/"
 
-sum_values = db.execute("SELECT SUM(amount), SUM(price) FROM log WHERE user_id = 1 and share = 'GOOG'")
-        
-# download average price from log
-avg_price = sum_values[0]["SUM(price)"] / sum_values[0]["SUM(amount)"]
-
-MAX = "(SELECT MAX(id) FROM log WHERE user_id = 1 and share = 'GOOG')"
-
-var = db.execute("SELECT amount, price, price_per_share FROM log WHERE user_id = 1 and id = (SELECT MAX(id) FROM log WHERE user_id = 1 and share = 'GOOG') and share = 'GOOG'")
-                    
-# count the average interest rate         
-interest_rate = update_cash["price"] * 100.0 / avg_price - 100.0
-
-filtered_interest = db.execute("SELECT SUM(interest), COUNT(interest) FROM portfolio WHERE user_id = 2")
+(db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash_1)", 
+        username=username, hash_1=hash))
